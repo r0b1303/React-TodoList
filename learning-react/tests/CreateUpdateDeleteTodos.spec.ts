d367d540685d47abc119ab5474f8d73fc819d2d6
rob <77494982+r0b1303@users.noreply.github.com>
2022-09-19 17:10:31 +0200

    Serial mode activated

--- learning-react/tests/CreateUpdateDeleteTodos.spec.ts
+++ learning-react/tests/CreateUpdateDeleteTodos.spec.ts
@@ -1,40 +1,45 @@
-import { test, expect, chromium } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 import { TodoForm } from "./TodoForm.Fixture";
 import { TodoList } from "./TodoList.Fixture";
 
 /*
 
 Add:
-  Serial Mode needs to be added;
   Localstorage check needs to be added;
 
 Bugs:
   Having 2 todos with the same name;
 
 */
+test.describe.configure({ mode: "serial" });
+
+let page: Page;
 
 test.describe("Create / Update / Delete Todos", () => {
-  test.describe.configure({ mode: "serial" });
   const PLACEHOLDER = "placeholder";
   const todoName = "Thats a test 123";
   let todoForm;
   let todoList;
   const todos = 2;
 
-  test.beforeEach(async ({ page }) => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test("should create a todo", async () => {
     todoForm = new TodoForm(page);
-    todoList = new TodoList(page);
 
     await page.goto("https://robderbob.netlify.app/");
     await expect(page).toHaveTitle("React App");
 
+    // create items
     await todoForm.fill(PLACEHOLDER);
     await todoForm.submit();
     await todoForm.fill(todoName);
     await todoForm.submit();
-  });
-
-  test("should create a todo", async ({ page }) => {
     // find the item
     const list = page.locator(`li`);
     await expect(list).toHaveCount(todos);
@@ -52,7 +57,9 @@ test.describe("Create / Update / Delete Todos", () => {
     //await expect(page.locator("text=" + todoName)).toBeVisible();
   });
 
-  test("should mark a todo as completed", async ({ page }) => {
+  test("should mark a todo as completed", async () => {
+    todoList = new TodoList(page);
+
     // find the Item
     const todo = await todoList.findItem(todoName);
 
@@ -61,20 +68,20 @@ test.describe("Create / Update / Delete Todos", () => {
     await expect(todo).toHaveClass(/completed/);
   });
 
-  test("should mark a complete todo as uncompleted", async ({ page }) => {
+  test("should mark a complete todo as uncompleted", async () => {
+    todoList = new TodoList(page);
+
     // find the item
     const todo = await todoList.findItem(todoName);
-
-    // check item
-    await todoList.checkTodo(todoName);
-    await expect(todo).toHaveClass(/completed/);
 
     // uncheck item
     await todoList.checkTodo(todoName);
     await expect(todo).not.toHaveClass(/completed/);
   });
 
-  test("should delete a todo", async ({ page }) => {
+  test("should delete a todo", async () => {
+    todoList = new TodoList(page);
+
     // find the item
     const list = await page.locator(".todo-item");
 
